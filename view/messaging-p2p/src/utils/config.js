b7a61ce1c8b0102d490e9af4c98182438fa2cfd7
Hamamura-S <sehamamura@gmail.com>
2024-08-22 14:51:15 -0300

    deploy react

--- view/messaging-p2p/src/utils/config.js
+++ view/messaging-p2p/src/utils/config.js
@@ -1,29 +1,28 @@
-
-const server_ip = '192.168.1.102'
+const server_ip = '192.168.1.102'; // Puedes eliminar esto si ya no lo necesitas
 
 const dev = {
-  API_URL: 'http://'+server_ip+':8080',
-  WS_URL: 'ws://'+server_ip+':8080'
+  API_URL: `http://${server_ip}:8080`,
+  WS_URL: `ws://${server_ip}:8080`,
 };
-  
+
 const prod = {
-  API_URL: 'https://api.midominio.com',
+  API_URL: `http://${process.env.REACT_APP_API_URL}` || 'http://localhost:8080'
 };
-  
+
 const staging = {
   API_URL: 'https://staging-api.midominio.com',
 };
-  
-  const config = process.env.REACT_APP_STAGE === 'production'
-    ? prod
-    : process.env.REACT_APP_STAGE === 'staging'
-      ? staging
-      : dev;
-  
-  const exportedConfig = {
-    ...config,
-  };
-      
+
+const config = process.env.REACT_APP_STAGE === 'production'
+  ? prod
+  : process.env.REACT_APP_STAGE === 'staging'
+    ? staging
+    : dev;
+
+const exportedConfig = {
+  ...config,
+};
+
 export default exportedConfig;
 
 export const API_URL = config.API_URL;
